210af813dd17b24ed470d9b4a2d5a4c50bc7151e
NotOfficals <danyabrotvyt@gmail.com>
2024-09-04 07:00:43 +0300

    Init

---
+++ src/main/java/not/notoffical/NotResourcePack.java
@@ -0,0 +1,16 @@
+package not.notoffical;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NotResourcePack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
